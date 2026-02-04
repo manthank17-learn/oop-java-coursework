dd8dca8f65dcc18367114ca72b148c9809ad429a
Manthan Kumar <manthanlearn@gmail.com>
2026-02-04 12:59:15 +0530

    Initial project setup

---
+++ src/oop/java/coursework/OopJavaCoursework.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.java.coursework;
+
+/**
+ *
+ * @author hp
+ */
+public class OopJavaCoursework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
